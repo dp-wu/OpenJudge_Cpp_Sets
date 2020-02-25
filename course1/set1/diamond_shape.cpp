09fcc3b3bdab8c1964c44aba27fa845a3abf5fc3
Yves <37640312+dp-wu@users.noreply.github.com>
2020-02-24 17:18:35 -0800

    create diamond_shape file

---
+++ course1/set1/diamond_shape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+
+    cout << "  " << a << "  \n";
+    cout << " " << a << a << a << " \n";
+    cout << a << a << a << a << a << '\n';
+    cout << " " << a << a << a << " \n";
+    cout << "  " << a << "  \n";
+}
