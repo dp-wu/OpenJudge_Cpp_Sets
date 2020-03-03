40d504222f50d60ef1518a2758d47d0808899d82
Yves <37640312+dp-wu@users.noreply.github.com>
2020-03-02 18:27:16 -0800

    Create a_powof_n.cpp

---
+++ course1/set3/a_powof_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n, result = 1;
+    cin >> a >> n;
+
+    while (n > 0) {
+      result *= a;
+      --n;
+    }
+
+    cout << result << endl;
+}
