57934f87cde831ea5f733d9a6ea9e51212bfc907
Yves <37640312+dp-wu@users.noreply.github.com>
2020-03-16 15:17:30 -0700

    Update bitManipulation1.cpp

--- course1/set6/bitManipulation1.cpp
+++ course1/set6/bitManipulation1.cpp
@@ -1,1 +1,20 @@
+/* 头秃练习 */
 
+#include <iostream>
+using namespace std;
+
+int bitManipulation1(int n, int m, int i) {
+  // 在此处补充你的代码
+  // 使得函数返回值为一个整数,该整数的第i位和m的第i位相同，其他位和n相同。
+  return (((1<<i) & m) + ((~(1<<i)) & n));
+}
+
+int main() {
+	int n, m, i, t;
+	cin >> t;
+	while (t--) {
+		cin >> n >> m >> i;
+		cout << bitManipulation1(n, m, i) << endl;
+	}
+	return 0;
+}
