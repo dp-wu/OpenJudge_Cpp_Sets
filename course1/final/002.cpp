39e945e108d0dfa9acf081b6008db6ac174ae535
Yves <37640312+dp-wu@users.noreply.github.com>
2020-05-29 17:58:16 -0700

    Create 002.cpp

---
+++ course1/final/002.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+double recur(int num, double p, double q, double sum) {
+    if (num == 0) return sum;
+    else {
+        sum += q / p;
+        return recur(--num, q, q+p, sum);
+    }
+}
+
+
+int main() {
+    double result=0;
+    int n, p0=1, q0=2;
+    scanf("%d", &n);
+    
+    result = recur(n, p0, q0, result);
+    printf("%.4f\n", result);
+}
