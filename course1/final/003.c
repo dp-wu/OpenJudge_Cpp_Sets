cd69b0118f909c72968deaf4108ed2de2e60b199
Yves <37640312+dp-wu@users.noreply.github.com>
2020-05-29 19:22:48 -0700

    Create 003.c

---
+++ course1/final/003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    int h, w, fill;
+    char c, fr=' ';
+    scanf("%d%d %c %d", &h, &w, &c, &fill);
+    
+    if (fill == 1) fr = c;
+    for (int row=0; row<h; ++row) {
+        for (int col=0; col<w; ++col) {
+            if ((row>0 && row<h-1) && (col>0 && col<w-1)) printf("%c", fr);
+            else printf("%c", c);
+        }
+        printf("\n");
+    }
+}
